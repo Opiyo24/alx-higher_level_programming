6dc12da93ec18196c95458ee0935af95f9a82831
Opiyo24 <gabbynyangor@gmail.com>
2023-08-08 16:31:20 +0000

    Additional

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,2 +1,4 @@
+#!/usr/bin/python3
 def add(a, b):
+    """returns addition of a and b"""
     return(a + b)
--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def pow(a,b):
+    """returns a to power b"""
     return (a**b)
