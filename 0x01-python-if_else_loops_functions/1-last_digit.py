2efbb4796026babe6da71eda9649e2b138f1ebd7
Opiyo24 <gabbynyangor@gmail.com>
2023-08-08 15:48:34 +0000

    While day's work

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str_num = str(number)
+length = (len(str_num) - 1)
+cmpr = int(str_num[length])
+if cmpr > 5:
+    print(f"Last digit of {number:d} is {cmpr:d} and is greater than 5")
+elif cmpr == 0:
+    print(f"Last digit of {number:d} is {cmpr:d} and is 0")
+elif cmpr < 6 and cmpr != 0:
+    print(f"Last digit of {number:d} is {cmpr:d} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(str):
+    cnt = int(len(str))
+    for i in range(cnt):
+        if (int(ord(str[i]))) >= 93 and i <= 123:
+            upp = (int(ord(str[i])) - 32)
+            print(chr(upp), end = "")
+        else:
+            print(str[i], end = "")
+    print(end = "\n")
