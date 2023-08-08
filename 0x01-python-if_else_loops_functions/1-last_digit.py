81ea1635b23279cb6fa1261b2d2ec6f80635441e
Opiyo24 <gabbynyangor@gmail.com>
2023-08-08 16:02:54 +0000

    Correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,8 @@ if cmpr > 5:
     print(f"Last digit of {number:d} is {cmpr:d} and is greater than 5")
 elif cmpr == 0:
     print(f"Last digit of {number:d} is {cmpr:d} and is 0")
+elif cmpr < 0:
+    cmpr = (-cmpr)
+    print(f"Last digit of {number:d} is {cmpr:d} and is less than 6 and not 0")
 elif cmpr < 6 and cmpr != 0:
     print(f"Last digit of {number:d} is {cmpr:d} and is less than 6 and not 0")
