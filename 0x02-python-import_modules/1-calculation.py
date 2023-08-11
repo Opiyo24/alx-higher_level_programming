bd3dad4b17e26d2fffc8141004bcdfd8e84f1726
Opiyo24 <gabbynyangor@gmail.com>
2023-08-10 23:21:31 -0700

    More and more corrections

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,24 +4,14 @@ if __name__ == "__main__":
     """Print trhe sum, difference, multiple and quotient of 10 and 5"""
     from calculator_1 import add, sub, mul, div
 
-    """My operation functions
-    """
     a = 10
     b = 5
-    """addition"""
+
     addition = add(a, b)
     print(f"{a:d} + {b:d} = {addition:d}")
-
-    """Subtraction"""
     subtraction = sub(a, b)
     print(f"{a:d} - {b:d} = {subtraction:d}")
-
-    """Multiplication"""
     multiplication = mul(a, b)
     print(f"{a:d} * {b:d} = {multiplication:d}")
-
-    """Division"""
     division = div(a, b)
     print(f"{a:d} / {b:d} = {division:0.0f}")
-    """Cast division to 0 decimal float
-    python defaults divisions to float type"""
