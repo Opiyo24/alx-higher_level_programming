9b40847d534ccdb840557ccbcbdb600576b95ffb
Opiyo24 <gabbynyangor@gmail.com>
2023-08-11 12:40:00 +0300

    Checkers

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,13 +6,12 @@ if __name__ == "__main__":
 
     args = sys.argv
     length = len(args)
-    
+
     if length == 1:
-        print("0 arguments.")
+        print("{} argument.".format(length - 1))
     elif length == 2:
-        print("1 argument:")
-        print("{}: {}".format(length, args[1]))
+        print("{} argument:".format(length -1))
     else:
-        print("{} arguments".format(length - 1))
-        for i in range(1, length):
-            print("{}: {}".format((i), args[i]))
+        print("{} arguments:".format(length -1))
+    for i in range(1, length):
+        print("{}: {}".format(i, args[i]))
