2b1a52a334d497889ce6c603af6f27bf9619b55a
Opiyo24 <gabbynyangor@gmail.com>
2023-08-11 00:09:57 -0700

    Debugging

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     args = sys.argv
     length = len(args)
 
-    if length == 0:
+    if length == 1:
         print("0 arguments")
-    elif length == 1:
+    elif length == 2:
         print("1 argument")
-        print("{}: {}".format(length, args[0]))
+        print("{}: {}".format(length, args[1]))
     else:
-        print("{} arguments".format(length))
-        for i in range(length):
-            print("{}: {}".format((i + 1), args[i]))
+        print("{} arguments".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format((i), args[i]))
