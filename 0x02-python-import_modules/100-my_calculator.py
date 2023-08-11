43cd87d48c8976a28a61d65700c9f613916553e2
Opiyo24 <gabbynyangor@gmail.com>
2023-08-11 14:47:43 +0300

    More calculations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-if __name__ == "__Main__":
-    """Imports all fuctions from calculator_1.py"""
-    """Check number of arguments"""
-    import sys
-    from calculator_1 import add, sub, mul, div
 
+if __name__ == "__main__":
+    """Imports all fuctions from calculator_1.py"""
+
+    from calculator_1 import add, sub, mul, div
+    import sys
+    
     argument = sys.argv
     length = len(argument)
     
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
     else:
         argument[1] = int(argument[1])
         argument[3] = int(argument[3])
         ans = 0
+
         """addition"""
         if argument[2] == "+":
             operator = "+"
             ans = add(argument[1], argument[3])
+
         elif argument[2] == "-":
             operator = "-"
             ans = sub(argument[1], argument[3])
+
         elif argument[2] == "*":
             operator = "*"
             ans = mul(argument[1], argument[3])
+
         elif argument[2] == "/":
             operator = "/"
             ans = div(argument[1], argument[3])
+
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
