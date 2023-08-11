34f111e261d352db090d8c3078a62c43f3f6337e
Opiyo24 <gabbynyangor@gmail.com>
2023-08-10 20:14:24 -0700

    New line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-import sys
-"""My function prints the number of arguments
-passed to it in the command line
-"""
-args = sys.argv
-#print(args)
-length = len(args)
+if __name__ == "__main__":
+    import sys
+    """My function prints the number of arguments
+    passed to it in the command line
+    """
+    args = sys.argv
+    #print(args)
+    length = len(args)
 
-if length == 0:
-    print("0 arguments")
-elif length == 1:
-    print("1 argument")
-    print(f"{length:d}: {args[0]}")
-else:
-    print(f"{length:d} arguments")
-    for i in range(length):
-        print(f"{(i + 1):d}: {args[i]}")
+    if length == 0:
+        print("0 arguments")
+    elif length == 1:
+        print("1 argument")
+        print(f"{length:d}: {args[0]}")
+    else:
+        print(f"{length:d} arguments")
+        for i in range(length):
+            print(f"{(i + 1):d}: {args[i]}")
