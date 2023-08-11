5142d8122844813bc421240d03b32f7477efac2a
Opiyo24 <gabbynyangor@gmail.com>
2023-08-11 14:17:06 +0300

    Debbugg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     elif length == 2:
         print("{} argument:".format(length -1))
     else:
-        print("{} arguments:".format(length -1))
+        print("{} arguments:".format(length - 1))
     for i in range(1, length):
         print("{}: {}".format(i, args[i]))
