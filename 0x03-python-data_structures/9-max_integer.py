00a9fbdce83c108bc20d6c54ab93010ccee2e100
Opiyo24 <gabbynyangor@gmail.com>
2023-08-13 22:39:55 +0300

    7, 8, 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,9 +4,9 @@ def max_integer(my_list = []):
     if len(my_list) < 1:
         return None
     else:
-        placeholder = 0
+        placeholder = my_list[0]
 
-        for i in range(len(my_list) - 1):
+        for i in range(len(my_list)):
             if my_list[i] > placeholder:
                 placeholder = my_list[i]
         return placeholder
