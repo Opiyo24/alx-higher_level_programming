c105e7c99aea7ed2c680623d467977165c780acd
Opiyo24 <gabbynyangor@gmail.com>
2023-08-29 14:56:26 +0300

    Growing the square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,6 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-    try:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
