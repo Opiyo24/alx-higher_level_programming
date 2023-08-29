6f4661c241447baa42cd4365c6234bbee6ec885f
Opiyo24 <gabbynyangor@gmail.com>
2023-08-29 14:38:25 +0300

    More of the square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-"""square - a class square"""
+
+"""square - a class square."""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
