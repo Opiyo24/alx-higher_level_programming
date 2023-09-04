eca26af6d8dc1147b239f0fac2a00e6f96b1b816
Opiyo24 <gabbynyangor@gmail.com>
2023-09-04 17:46:03 +0300

    Printing squares

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -71,3 +71,15 @@ class Rectangle:
         if ((self.__width == 0) or (self.__height == 0)):
             return (0)
         return (2 * (self.__height + self.__width))
+    
+    def __str__(self):
+        """"""
+        if self.__width ==0 or self.__height == 0:
+            return("")
+        
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
