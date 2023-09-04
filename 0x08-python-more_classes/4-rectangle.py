c8057b9cc9b5899183206923d32ecc0f9f50ddc8
Opiyo24 <gabbynyangor@gmail.com>
2023-09-04 18:05:15 +0300

    Rectangular Repr

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -83,3 +83,9 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+    
+    def __repr__(self):
+        """Print string representation of Rectangle"""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
