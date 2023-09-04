386c756d15e97c2c8564eacca86517257d1b3336
Opiyo24 <gabbynyangor@gmail.com>
2023-09-04 15:08:26 +0300

    Create empty rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """A class rectangle"""
 class Rectangle:
-    """An empty class Rectangle"""
+    """An empty class Rectangle
+    Args:
+        None
+    Return: 
+        None"""
     pass
