166f616cf94756f20ca67572ed7100e80095c279
Opiyo24 <gabbynyangor@gmail.com>
2023-09-11 11:47:09 +0000

    Instances of mothers

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module checks if an object is an instance of
+    a class or an instance of an object that is an instance
+    of a class
+"""
+def is_kind_of_class(obj, a_class):
+    """is_kind_of_class - checks if an object is an instance
+    Args:
+        obj: object in question
+        a_class: the parent object
+    return: True if is instance or false
+    """
+    if isinstance(object, a_class):
+        return True
+    else:
+        return False
