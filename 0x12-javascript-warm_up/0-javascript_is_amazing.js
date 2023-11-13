17bc91baa7a9aab961646de3ae29b0f2cf133a52
Opiyo24 <gabbynyangor@gmail.com>
2023-11-13 19:51:53 +0300

    First two files

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ * Javascript stuff
+ * Ausio
+*/
+const myVar = "JavaScript is amazing";
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/*
+ * Print multiline
+*/
+console.log('C is fun'\n'Python is cool'\n'JavaScript is amazing');
