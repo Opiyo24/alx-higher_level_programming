9f0840b24ef78fdb737e677172db0c5b06a0fa82
Opiyo24 <gabbynyangor@gmail.com>
2023-11-13 19:56:26 +0300

    Number 3

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ * Arguments
+*/
+const number = process.argv.length - 2;
+
+if (number === 0) {
+	console.log('No argument');
+} else if (number === 1) {
+	console.log('Argumrnt found');
+} else {
+	console.log('Arguments found');
+}
