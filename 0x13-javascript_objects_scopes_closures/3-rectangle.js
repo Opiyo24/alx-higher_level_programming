da7fa0a4a9ad4530701de5f64baf01f47ab100e9
Opiyo24 <gabbynyangor@gmail.com>
2023-11-14 18:33:07 +0300

    Print rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,10 +6,14 @@ class Rectangle {
       this.height = h;
     }
   }
-}
-function print() {
-  for (let i = 0; i < this.height; i++) {
-    console.log(`X`.repeat(this.width));
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
   }
 }
 
